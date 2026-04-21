11d08fbe3bb5d4c4fed52f034ebbf5a89a56a993
Majoodeh <Majdhussein144@gmail.com>
2026-04-21 12:40:34 +0200

    feat: setup navbar, hompage

---
+++ client/src/components/RateLimitedUI.tsx
@@ -0,0 +1,30 @@
+import { ZapIcon } from "lucide-react";
+import React from "react";
+
+const RateLimitedUI = () => {
+  return (
+    <div className="mx-auto px-4 py-8 max-w-6xl">
+      <div className="bg-accent/10 shadow-md border border-accent/50 rounded-lg">
+        <div className="flex md:flex-row flex-col items-center p-6">
+          <div className="bg-accent/20 md:mr-6 mb-4 md:mb-0 p-4 rounded-full shrink-0">
+            <ZapIcon className="size-10 text-accent" />
+          </div>
+          <div className="flex-1 md:text-left text-center">
+            <h3 className="mb-2 font-bold text-xl"> Rate Limit Reached</h3>
+            <p className="mb-1 text-base-content">
+              {" "}
+              You have made too many requests in a short period. Please wait a
+              moment before trying again.{" "}
+            </p>
+            <p className="text-sm text-base-content/70">
+              {" "}
+              Try again in a few seconds{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RateLimitedUI;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        console.log("response=====>    ", res);
+      } catch (error) {
+        console.log("Error Fetching Notes:  ", error);
+      }
+    };
+  });
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      {/* // Conditional rendering of the */}
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
 };
 
 export default HomePage;
