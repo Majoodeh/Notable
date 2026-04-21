870885fe8c1485afd97526733ad0f5962579ec2c
Majoodeh <Majdhussein144@gmail.com>
2026-04-21 21:27:59 +0200

    refactor: refactor server settings

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -10,6 +10,7 @@ const rateLimiter = async (
     const { success } = await ratelimit.limit(
       "userId when there is authentication, otherwise use IP address",
     );
+    // ! TODO: Implement a more robust key generation strategy for authenticated users, because now it is just using a static string which will not differentiate between different users or IPs. Consider using req.ip for unauthenticated users and a unique identifier from the authentication token for authenticated users.
     if (!success) {
       res
         .status(429)
